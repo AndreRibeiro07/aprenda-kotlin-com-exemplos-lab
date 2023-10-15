eade28f23db0ed9485a5d168124c0260f577489c
André Ribeiro <86237536+AndreRibeiro07@users.noreply.github.com>
2023-10-15 19:38:52 -0300

    Finalizando o desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,70 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class ConteudoEducacional(
+    val nome : String,
+    val url: String
+)
 
-class Usuario
+data class Aluno(
+    val nome: String,
+    val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(
+    val nome: String,
+    val nivel: String,
+    val conteudosEducacionais: MutableList<ConteudoEducacional> = mutableListOf(),
+    val alunosMatriculados: MutableList<Aluno> = mutableListOf()
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Curso(val formacoes: MutableList<Formacao> = mutableListOf(), val alunos: MutableList<Aluno> = mutableListOf() ) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun criarFormacao(nome : String, nivel : String){
+        val formacao = Formacao(nome, nivel)
+        formacoes.add(formacao)
+    }
+
+    fun adicionarConteudoFormacao(formacao: Formacao, conteudo: ConteudoEducacional){
+    formacao.conteudosEducacionais.add(conteudo)
+    }
+
+    fun matricularAlunoEmFormacao(formacao: Formacao, aluno: Aluno){
+        formacao.alunosMatriculados.add(aluno)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val curso = Curso()
+    
+    curso.criarFormacao("Iniciando o curso de Kotlin", "Nivel Básico")
+    curso.criarFormacao("Avançando em Kotlin", "Intermediário")
+    
+    val conteudo1 = ConteudoEducacional("Introdução", "https://kotlinlang.org")
+    val conteudo2 = ConteudoEducacional("Continuando em Java", "https://dev.java")
+    curso.adicionarConteudoFormacao(curso.formacoes[0], conteudo1)
+    curso.adicionarConteudoFormacao(curso.formacoes[1], conteudo2)
+
+    val aluno1 = Aluno ("Felipe Toledo", "devfelitol@hotmail.com")
+    val aluno2 = Aluno ("Marcos Rocha", "devmarrocha@outlook.com")
+    val aluno3 = Aluno ("Josh Craig", "devjosh@gmail.com")
+    
+    curso.matricularAlunoEmFormacao(curso.formacoes[0], aluno1)
+    curso.matricularAlunoEmFormacao(curso.formacoes[0], aluno2)
+    curso.matricularAlunoEmFormacao(curso.formacoes[1], aluno3)
+    
+     println("Formações:")
+        
+    curso.formacoes.forEach { formacao ->
+        println("Nome: ${formacao.nome}, Nível: ${formacao.nivel}")
+        println("Conteúdos Educacionais:")
+        formacao.conteudosEducacionais.forEach { conteudo ->
+            println(" - Nome: ${conteudo.nome}, URL: ${conteudo.url}")
+        }
+
+        println("Alunos Matriculados:")
+        formacao.alunosMatriculados.forEach { aluno -> 
+            println(" - Nome: ${aluno.nome}, Email: ${aluno.email}")
+        }
+    }
+
 }
